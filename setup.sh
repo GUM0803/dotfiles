b1a888a35a02d08b39b108109a90609a849a2014
tig <tig@example.com>
2017-01-15 01:48:04 +0900

    vimをセットアップするスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install -y curl vim gcc make &&\
+cd ~/dotfiles &&\
+cp .vimrc ~/ &&\
+cd ~/.cache &&\
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh &&\
+sh ./installer.sh dein
+vim ~/dotfiles/setup.sh
